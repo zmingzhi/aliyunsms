a4465bf038a73ae5ac34641f8ea41ca3fc620901
zackzhong <zmingzhi@yeah.net>
2017-06-28 12:11:11 +0800

    first commit

---
+++ src/build/Base.php
@@ -0,0 +1,74 @@
+<?php
+namespace zongphp\aliyunsms\build;
+include_once __DIR__ . DIRECTORY_SEPARATOR . 'aliyun-php-sdk-core/Config.php';
+use zongphp\config\Config;
+use Sms\Request\V20160927 as Sms;
+
+/**
+ * 阿里云短信处理
+ * Class Base
+ * @package houdunwang\aliyunsms\build
+ */
+class Base {
+	protected $error;
+
+	/**
+	 * 发送短信
+	 *
+	 * @param array $data
+	 * $data=[
+	 *  'code'=>'模板code',
+	 *  'mobile'=>'手机号',
+	 *  //以下是模板中的变量
+	 *  'vars'=>[
+	 *      'code'=>333,
+	 *  ]
+	 * ]
+	 *
+	 * @return bool
+	 */
+	public function send( array $data ) {
+		chdir( __DIR__ );
+		$iClientProfile = \DefaultProfile::getProfile( "cn-hangzhou",
+			Config::get( 'aliyunsms.accessKey' ),
+			Config::get( 'aliyunsms.accessSecret' )
+		);
+		$client         = new \DefaultAcsClient( $iClientProfile );
+		$request        = new Sms\SingleSendSmsRequest();
+
+		/*签名名称*/
+		$request->setSignName( Config::get( 'aliyunsms.sign' ) );
+		/*模板code*/
+		$request->setTemplateCode( $data['template_code'] );
+		/*目标手机号*/
+		$request->setRecNum( $data['mobile'] );
+		$request->setParamString( json_encode( $data['vars'], JSON_UNESCAPED_UNICODE ) );
+
+		chdir( dirname( $_SERVER['SCRIPT_FILENAME'] ) );
+		try {
+			$client->getAcsResponse( $request );
+
+			return true;
+		} catch ( \ClientException  $e ) {
+//			print_r( $e->getErrorCode() );
+//			print_r( $e->getErrorMessage() );
+			$this->error = $e->getErrorMessage();
+
+			return false;
+		} catch ( \ServerException  $e ) {
+//			print_r( $e->getErrorCode() );
+//			print_r( $e->getErrorMessage() );
+			$this->error = $e->getErrorMessage();
+
+			return false;
+		}
+	}
+
+	/**
+	 * 获取错误信息
+	 * @return mixed
+	 */
+	public function getError() {
+		return $this->error;
+	}
+}
